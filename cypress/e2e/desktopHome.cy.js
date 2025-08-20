2372035d57c1538faf32756ec498502e24948ede
Madhumini98 <mkkodithuwakku@std.appsc.sab.ac.lk>
2025-08-20 17:23:40 +0530

    TC_GALUMA_FOOTER_LINKS_020 - Ensure all footer links redirect to correct respective pages

--- cypress/e2e/desktopHome.cy.js
+++ cypress/e2e/desktopHome.cy.js
@@ -17,7 +17,17 @@ describe('Galuma Home Tests', () => {
         cy.title().should('not.be.empty')
     })
 
-    it('TC_GALUMA_CART_MOBILE_003 - Verify cart icon opens cart popup and close icon returns to homepage', () => {
+    it('TC_GALUMA_CART_002 - Verify track icon opens track your order page', () => {
+        // Click on the track icon 
+        cy.get('#track').should('be.visible').click()
+
+        // Verify navigation to track order page
+        cy.url().should('include', '/track-my-order')
+        cy.get('body').should('be.visible')
+        cy.wait(2000)
+    })
+
+    it('TC_GALUMA_CART_003 - Verify cart icon opens cart popup and close icon returns to homepage', () => {
         // Click on the cart icon to open cart popup
         cy.get('#open-cart-popup > .img-fluid').should('be.visible').click()
 
@@ -141,6 +151,337 @@ describe('Galuma Home Tests', () => {
         cy.go('back')
     })
 
-    
+    it('TC_GALUMA_PAYMENTPLANS_014 - Verify Payment Plans section displays content and View Options button functions correctly', () => {
+        // Verify Payment Plans section is visible 
+        cy.get('.pay_plan').should('be.visible')
+        cy.wait(2000)
+
+        // Verify section content is displayed 
+        cy.get('.pay_plan').within(() => {
+            // Look for financing related text (based on actual content)
+            cy.get('*').should('contain.text', 'finance')
+            cy.get('*').should('contain.text', 'Afterpay')
+            cy.get('.dis-flex-start > .btn').should('be.visible').and('contain.text', 'View Options')
+        })
+
+        // Click View Options button and verify navigation
+        cy.get('.dis-flex-start > .btn').click()
+        cy.wait(2000)
+
+        // Verify navigation to payment options page
+        cy.url().should('include', '/payments/options')
+
+        // Verify payment options page content
+        cy.get('body').should('be.visible')
+
+        // Verify detailed payment information is displayed
+        cy.get('body').should('contain.text', 'payment')
+
+        // Go back to homepage
+        cy.go('back')
+        cy.wait(1000)
+    })
+
+    it('TC_GALUMA_PAYMENTICONS_015 - Ensure payment option icons navigate to respective payment pages', () => {
+
+        /*
+        // Test Stripe icon navigation - Not working properly
+        cy.get('.pays > :nth-child(3) > .st').should('be.visible')
+        cy.get('.pays > :nth-child(3) > .st').should('have.attr', 'href').then((href) => {
+            cy.get('.pays > :nth-child(3) > .st').click()
+            cy.wait(2000)
+
+            // Verify URL after clicking Stripe icon
+            cy.url().should('include', 'galumatires.com')
+
+            // Navigate back to homepage for next test
+            cy.visit("https://dev.galumatires.com/", {
+                auth: {
+                    username: 'galumadev',
+                    password: 'Test.123'
+                }
+            })
+            cy.wait(2000)
+            cy.go('back')
+        })
+        */
+
+        // Test Affirm icon navigation
+        cy.get(':nth-child(14) > .container > .row > .col > .content').scrollIntoView()
+        cy.get(':nth-child(14) > .container > .row > .col > .content > .pays > :nth-child(1) > a > .aff').should('be.visible')
+        cy.get(':nth-child(14) > .container > .row > .col > .content > .pays > :nth-child(1) > a').should('have.attr', 'href').then((href) => {
+            cy.get(':nth-child(14) > .container > .row > .col > .content > .pays > :nth-child(1) > a > .aff').click()
+            cy.wait(2000)
+
+            // Verify URL after clicking Affirm icon - should go to /payments/affirm
+            cy.url().should('include', '/payments/affirm')
+
+            // Verify page content is relevant to Affirm
+            cy.get('body').should('be.visible')
+
+            // Navigate back to homepage for next test
+            cy.visit("https://dev.galumatires.com/", {
+                auth: {
+                    username: 'galumadev',
+                    password: 'Test.123'
+                }
+            })
+            cy.wait(2000)
+            cy.go('back')
+        })
+
+        /*
+        // Test Afterpay icon navigation - Not working properly
+        cy.get('.financing-container').scrollIntoView()
+        cy.get('.:nth-child(14) > .container > .row > .col > .content > .pays > :nth-child(2) > .af').should('be.visible')
+        cy.get(':nth-child(14) > .container > .row > .col > .content > .pays > :nth-child(2) > .af').should('have.attr', 'href').then((href) => {
+            cy.get('.:nth-child(14) > .container > .row > .col > .content > .pays > :nth-child(2) > .af').click()
+            cy.wait(2000)
+
+            // Verify URL after clicking Afterpay icon
+            cy.url().should('include', 'galumatires.com')
+
+            // Verify page content is visible
+            cy.get('body').should('be.visible')
+            cy.go('back')
+        })
+        */
+    })
+
+    it('TC_GALUMA_SERVICES_ALL_016 - Verify service tiles in "All services in our shop" section navigate to correct pages', () => {
+        // Scroll to "All services in our shop" section
+        cy.get(':nth-child(17) > .container > :nth-child(1) > .col-12').scrollIntoView()
+        cy.wait(2000)
+
+        // Test Touchless Installation tile
+        cy.get(':nth-child(1) > .col-12 > .services > :nth-child(1) > .img-box > a > .img-fluid').should('be.visible').scrollIntoView().click({ force: true })
+        cy.url().should('include', 'touchless-installation')
+        cy.go('back')
+        cy.wait(1000)
+
+        // Test Road Force Balance tile  
+        cy.get(':nth-child(1) > .col-12 > .services > :nth-child(2) > .img-box > a > .img-fluid').should('be.visible').scrollIntoView().click({ force: true })
+        cy.url().should('include', 'road-force-balance')
+        cy.go('back')
+        cy.wait(1000)
+
+        // Test Inground Alignment tile
+        cy.get(':nth-child(1) > .col-12 > .services > :nth-child(3) > .img-box > a > .img-fluid').should('be.visible').scrollIntoView().click({ force: true })
+        cy.url().should('include', 'inground')
+        cy.go('back')
+        cy.wait(1000)
+
+        /*this Center Lock tile is not working properly, so skipping this test for now
+        // Test Center Lock tile 
+        cy.get(':nth-child(1) > .col-12 > .services > :nth-child(4) > .img-box > a > .img-fluid').should('be.visible').scrollIntoView().click({ force: true })
+        cy.url().should('eq', 'https://dev.galumatires.com/')
+        cy.wait(1000)
+        */
+
+        // Test Tire Repair tile
+        cy.get(':nth-child(1) > .col-12 > .services > :nth-child(5) > .img-box > a > .img-fluid').should('be.visible').scrollIntoView().click({ force: true })
+        cy.url().should('include', 'puncture-repair')
+        cy.go('back')
+        cy.wait(1000)
+
+        // Test Tire Sales tile
+        cy.get(':nth-child(1) > .col-12 > .services > :nth-child(6) > .img-box > a > .img-fluid').should('be.visible').scrollIntoView().click({ force: true })
+        cy.url().should('include', '/t')
+        cy.go('back')
+    })
+
+    it('TC_GALUMA_SHIPPINGSECTION_MOBILE_017 - Verify "Our shipping" section displays content and Read More button works', () => {
+        // Navigate directly to the shipping section
+        cy.get(':nth-child(17) > .container > :nth-child(2) > .col > .content').scrollIntoView()
+        cy.wait(2000)
+
+        // Verify the shipping section is visible
+        cy.get(':nth-child(17) > .container > :nth-child(2) > .col > .content').should('be.visible')
+
+        // Verify shipping section content elements are displayed
+        cy.get(':nth-child(17) > .container > :nth-child(2) > .col > .content').within(() => {
+
+            // Verify Read More button is visible and clickable
+            cy.get('p > .btn').should('be.visible')
+        })
+
+        // Test Read More button functionality
+        cy.get('p > .btn').should('be.visible').click({ force: true })
+
+        // Verify navigation to shipping details page
+        cy.url().should('include', '/shipping')
+        cy.get('body').should('be.visible')
+
+        // Go back to homepage
+        cy.go('back')
+    })
+
+    it('TC_GALUMA_PICKUPORDER_018 - Ensure Pick Up your order section displays clear steps and Find More button works', () => {
+        // Scroll to the "Pick Up your order & save!" section
+        cy.get('.pick_order > .container').scrollIntoView()
+        cy.wait(2000)
+
+        // Verify the Pick Up your order section is visible
+        cy.get('.pick_order > .container').should('be.visible')
+
+        // Verify the section contains meaningful content
+        cy.get('.pick_order > .container').within(() => {
+            // Check for pickup related text
+            cy.get('*').should('contain.text', 'Pick')
+        })
+
+        // Tap the "Find More" button
+        cy.get('.content > .find_more_btn > #findMore-btn').should('be.visible').click()
+        cy.wait(2000)
+
+        // Confirm navigation to the "Pick Up your tires" page
+        cy.url().should('include', '/pick-up')
+
+        // Verify the page content is visible and contains pickup information
+        cy.get('body').should('be.visible')
+        cy.get('body').should('contain.text', 'pick')
+
+        // Go back to homepage
+        cy.go('back')
+    })
+
+    it('TC_GALUMA_HELPADVICE_019 - Ensure Help & advice section displays correct content and Click to Contact button works', () => {
+        // Locate the "Help & advice" section
+        cy.get('.help_desk.mobile-hide > .container').scrollIntoView()
+        cy.wait(2000)
+
+        // Verify the Help & advice section is visible
+        cy.get('.help_desk.mobile-hide > .container').should('be.visible')
+
+        // Tap the "Click to Contact" button
+        cy.get('.container > .content > .btn').click()
+        cy.wait(2000)
+
+        // Confirm navigation to the "How to contact us" page
+        cy.url().should('include', '/contact-us')
+
+        // Verify the contact page content is visible
+        cy.get('body').should('be.visible')
+        cy.get('body').should('contain.text', 'contact')
+
+        // Go back to homepage
+        cy.go('back')
+    })
+
+    it('TC_GALUMA_FOOTER_LINKS_020 - Ensure all footer links redirect to correct respective pages', () => {
+        // Scroll to the bottom of the page
+        cy.get('footer.main_footer > .pre_footer').scrollIntoView()
+        cy.wait(2000)
+
+        // Verify the bottom section is visible
+        cy.get('.pre_footer > .container > .row').should('be.visible')
+
+        // Test "My Account" link
+        cy.get('.first > :nth-child(1) > a').should('be.visible').click()
+        cy.wait(2000)
+
+        // Verify it redirects to the account page
+        cy.url().should('include', '/sign-in')
+        cy.get('body').should('be.visible')
+
+        // Go back to homepage
+        cy.visit("https://dev.galumatires.com/", {
+            auth: {
+                username: 'galumadev',
+                password: 'Test.123'
+            }
+        })
+        cy.wait(2000)
+
+        // Scroll to footer again and test "Track my order" link
+        cy.get('.pre_footer > .container > .row').scrollIntoView()
+        cy.get('.first > :nth-child(2) > a').should('be.visible').click()
+        cy.wait(2000)
+
+        // Verify the tracking page loads
+        cy.url().should('include', '/track-my-order')
+        cy.get('body').should('be.visible')
+
+        // Go back to homepage
+        cy.visit("https://dev.galumatires.com/", {
+            auth: {
+                username: 'galumadev',
+                password: 'Test.123'
+            }
+        })
+        cy.wait(2000)
+
+        // Scroll to footer again and test "Membership Benefits" link
+        cy.get('.pre_footer > .container > .row').scrollIntoView()
+        cy.get('.first > :nth-child(3) > a').should('be.visible').click()
+        cy.wait(2000)
+
+        // Verify the membership benefits page loads
+        cy.url().should('include', '/warranty')
+        cy.get('body').should('be.visible')
+
+        // Go back to homepage
+        cy.visit("https://dev.galumatires.com/", {
+            auth: {
+                username: 'galumadev',
+                password: 'Test.123'
+            }
+        })
+        cy.wait(2000)
+
+        // Scroll to footer again and test "About Us" link
+        cy.get('.pre_footer > .container > .row').scrollIntoView()
+        cy.get('.first > :nth-child(4)').should('be.visible').click()
+        cy.wait(2000)
+
+        // Verify the about us page loads
+        cy.url().should('include', '/about-us')
+        cy.get('body').should('be.visible')
+
+        // Go back to homepage
+        cy.visit("https://dev.galumatires.com/", {
+            auth: {
+                username: 'galumadev',
+                password: 'Test.123'
+            }
+        })
+        cy.wait(2000)
+
+        // Scroll to footer again and test "Contact Us" link
+        cy.get('.pre_footer > .container > .row').scrollIntoView()
+        cy.get('.first > :nth-child(5) > a').should('be.visible').click()
+        cy.wait(2000)
+
+        // Verify the Contact page loads
+        cy.url().should('include', '/contact-us')
+        cy.get('body').should('be.visible')
+
+        // Go back to homepage
+        cy.visit("https://dev.galumatires.com/", {
+            auth: {
+                username: 'galumadev',
+                password: 'Test.123'
+            }
+        })
+        cy.wait(2000)
+
+        // Scroll to footer again and test "Help Center" link
+        cy.get('.pre_footer > .container > .row').scrollIntoView()
+        cy.get('.first > :nth-child(6)').should('be.visible').click()
+        cy.wait(2000)
+
+        // Verify the Help Center page loads
+        cy.url().should('include', '/help-n-advice')
+        cy.get('body').should('be.visible')
+
+        // Go back to homepage
+        cy.visit("https://dev.galumatires.com/", {
+            auth: {
+                username: 'galumadev',
+                password: 'Test.123'
+            }
+        })
+        cy.wait(1000)
+    })
 
 })
