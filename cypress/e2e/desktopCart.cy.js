ab9213f040a2693f46771e3a10c1b676da7ee243
Madhumini98 <mkkodithuwakku@std.appsc.sab.ac.lk>
2025-08-27 17:25:14 +0530

    TC_GALUMA_DESKTOP_CART_001 - Verify user can able to add products to the cart

--- cypress/e2e/desktopCart.cy.js
+++ cypress/e2e/desktopCart.cy.js
@@ -12,64 +12,40 @@ describe('Galuma Cart Functionality Tests', () => {
   })
 
   it('TC_GALUMA_DESKTOP_CART_001 - Verify user can able to add products to the cart', () => {
-    // Navigate to shop tires page: https://dev.galumatires.com/t/s
-    cy.visit("https://dev.galumatires.com/t/s", {
+    // Navigate to the shop tires page with authentication
+    cy.visit('https://dev.galumatires.com/t/s', {
       auth: {
         username: 'galumadev',
         password: 'Test.123'
       }
     })
+    
+    // Wait for the page to load completely and products to be displayed
     cy.wait(3000)
-
-    // Verify navigation to shop tires page
-    cy.url().should('include', '/t/s')
-    cy.get('body').should('be.visible')
-
-    // Scroll to products section
-    cy.get('#tire-products-container').should('be.visible').scrollIntoView()
-    cy.wait(2000)
-
-    // Hover over product card to reveal overlay
-    cy.get('#tire-products-container :nth-child(2) > .box-cover').first().trigger('mouseenter', { force: true })
+    
+    // Scroll to the products section to ensure it's visible
+    cy.get('#tire-products-container').scrollIntoView()
     cy.wait(1000)
-
-    // Wait for products to load and get the first product randomly
-    cy.get('#tire-products-container [data-eid]').should('have.length.at.least', 1)
-    cy.get('#tire-products-container [data-eid]').then($products => {
-      const randomIndex = Math.floor(Math.random() * $products.length)
-      const $randomProduct = $products.eq(randomIndex)
-      const dataEid = $randomProduct.attr('data-eid')
-
-      // Hover over the selected product to reveal overlay
-      cy.get(`#tire-products-container > [data-eid="${dataEid}"] > .box-cover`).trigger('mouseenter', { force: true })
-      cy.wait(2000)
-
-      // Check if overlay exists and click view product button (handle opacity issue)
-      cy.get(`[data-eid="${dataEid}"] > .box-cover > .overlay`).should('exist')
-      cy.get(`[data-eid="${dataEid}"] > .box-cover > .overlay > .brand > .view_product`).click({ force: true })
-      cy.wait(3000)
-
-      // Verify navigation to product details page
-      cy.url().should('include', '/p/')
-      cy.get('body').should('be.visible')
-      cy.wait(2000)
-
-      // Click on 'Add to Cart' button with the updated selector
-      cy.get(':nth-child(1) > #add-to-cart-button').should('be.visible').click()
-      cy.wait(3000)
-
-      // Check product visibility on the cart using the specified selector
-      cy.get('.side-cart-inner').should('be.visible')
-      cy.get('.side-cart-inner').should('contain.text', 'Cart')
-
-      // Verify item is visible in cart
-      cy.get('.side-cart-inner').find('[class*="cart"], [class*="item"], .product, [class*="product"]').should('be.visible')
-      cy.wait(2000)
-
-      // Close the cart using the specified selector
-      cy.get('#close-cart-popup > strong').should('be.visible').click()
-      cy.wait(1000)
+    
+    // Get the first product and hover over it to make the Add to Cart button visible
+    cy.get('#tire-products-container > div:nth-child(1)').trigger('mouseover')
+    cy.wait(1000)
+    
+    // Verify the Add to Cart button becomes visible after hover
+    cy.get('.add-to-cart-btn').first().should('exist')
+    
+    // Click on the "Add to Cart" button in a visible manner
+    cy.get('.add-to-cart-btn').first().then($btn => {
+      // Log the button details for visibility
+      cy.log('Clicking Add to Cart button:', $btn.text())
     })
+    cy.get('.add-to-cart-btn').first().click({ force: true })
+    
+    // Verify that the cart sidebar opens and product is visible in the cart
+    cy.get('.side-cart-inner').should('be.visible')
+    
+    // Close the cart popup by clicking the close button
+    cy.get('#close-cart-popup > strong').should('be.visible').click()
   })
 
 })
